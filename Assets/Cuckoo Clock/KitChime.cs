7c1450cf2ae44f54c85122ac185cf33fc30ebaad
jhgh <92657664+stapleshgh@users.noreply.github.com>
2025-03-20 10:09:58 -0400

    cuckoo clock

---
+++ Assets/Cuckoo Clock/KitChime.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitChime : MonoBehaviour
+{
+    public void Chime()
+    {
+        Debug.Log("Chiming !");
+    }
+}
---
+++ Assets/Cuckoo Clock/KitClock.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KitClock : MonoBehaviour
+{
+    public float timeAnHourTakes = 5;
+
+    public Transform minuteHand;
+    public Transform hourHand;
+
+    public float t;
+    public int hour = 0;
+
+    public UnityEvent OnTheHour;
+
+    Coroutine ClockCoroutine;
+
+    void Start()
+    {
+        ClockCoroutine = StartCoroutine(MoveClock());
+    }
+
+    private IEnumerator MoveClock()
+    {
+
+        while (true)
+        {
+            yield return StartCoroutine(MoveTheClockHandsOneHour());
+        }
+       
+
+    }
+
+    private IEnumerator MoveTheClockHandsOneHour()
+    {
+        t = 0;
+        while(t < timeAnHourTakes)
+        {
+            t += Time.deltaTime;
+            minuteHand.Rotate(0, 0,  -(360 / timeAnHourTakes) * Time.deltaTime);
+            hourHand.Rotate(0, 0, -(30 / timeAnHourTakes) * Time.deltaTime);
+            yield return null;
+
+        }
+        OnTheHour.Invoke();
+    }
+}
