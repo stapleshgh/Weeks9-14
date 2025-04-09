81be7fbcae78f7a920c212943d848f1fdfa44089
staples <howardhickey11@gmail.com>
2025-04-09 00:54:54 -0400

    rect 2 rect collision works now

--- Assets/SYSTEM PROJECT/Scripts/PhysicsBuffer.cs
+++ Assets/SYSTEM PROJECT/Scripts/PhysicsBuffer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PhysicsBuffer : MonoBehaviour
 {
@@ -8,16 +9,24 @@ public class PhysicsBuffer : MonoBehaviour
 
     GameObject[] physicsObjects = null;
 
+    [SerializeField] UnityEvent<GameObject> onCollisionDetected = new UnityEvent<GameObject>();
+
     void Start()
     {
         physicsObjects = GameObject.FindGameObjectsWithTag("physicsObject");
+
+        foreach (var po in physicsObjects)
+        {
+            //physicsObject script = po.GetComponent<physicsObject>();
+            //onCollisionDetected.AddListener(script.onCollision);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
-
+        
         foreach (var po in physicsObjects)
         {
             checkCollision(po);
@@ -31,9 +40,13 @@ public class PhysicsBuffer : MonoBehaviour
         foreach (GameObject s2 in physicsObjects)
         {
             if (s1.transform.localPosition.x + (s1.transform.localScale.x / 2) > s2.transform.localPosition.x - (s2.transform.localScale.x / 2) &&     // r1 right edge past r2 left
-                s1.transform.localPosition.x - (s1.transform.localScale.x / 2) < s2.transform.localPosition.x + (s2.transform.localScale.x / 2))
+                s1.transform.localPosition.x - (s1.transform.localScale.x / 2) < s2.transform.localPosition.x + (s2.transform.localScale.x / 2) &&
+                s1.transform.localPosition.y + (s1.transform.localScale.y / 2) >= s2.transform.localPosition.y - (s2.transform.localScale.y/ 2) &&
+                s1.transform.localPosition.y - (s1.transform.localScale.y / 2) <= s2.transform.localPosition.y + (s2.transform.localScale.y / 2) && 
+                s1.name != s2.name)
             {
                 Debug.Log("colliding");
+                onCollisionDetected.Invoke(s1);
             } else
             {
                 
--- Assets/SYSTEM PROJECT/Scripts/physicsObject.cs
+++ Assets/SYSTEM PROJECT/Scripts/physicsObject.cs
@@ -1,41 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class physicsObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isFalling = true;
 
     public Vector2 velocity;
     void Start()
     {
-        onCollision();
+        //onCollision();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
-
-        if (transform.position.y - transform.localScale.y / 2 < -4)
+        if (isFalling)
         {
-            velocity.y = 0;
-            transform.position = new Vector2(transform.position.x, transform.position.y + 0.01f);
+            velocity.y += -0.01f * Time.deltaTime;
         } else
         {
-            velocity.y += -0.01f;
+            velocity.y = 0;
         }
 
         transform.position = new Vector2(transform.position.x + velocity.x, transform.position.y + velocity.y);
+        
+
     }
 
 
-    public void onCollision()
+    public void onCollision(GameObject s1)
     {
-        StartCoroutine(generateImpulse());
+        //if the collided object is me, stop falling
+        if (s1 == gameObject)
+        {
+            isFalling = false;
+        }
+        
     }
 
     IEnumerator generateImpulse()
